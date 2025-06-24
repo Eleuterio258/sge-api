174e0f2f2d02a82290eaffe042c8f0811c8483be
Eleuterio Fulaho Notico <eleuterio3d@gmail.com>
2025-06-24 09:18:38 +0200

    subir
    subir

--- api/config/db.js
+++ api/config/db.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST || "135.181.249.37",
     user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
+    password: process.env.DB_PASSWORD || "Maputo2023@#",
     database: process.env.DB_NAME || "sge_conducao",
     waitForConnections: true,
     connectionLimit: 10,
